e8b15f0879030d2e89325aec176ad9de67c286bd
heri <24690705@tecvalles.mx>
2025-04-16 13:46:11 -0600

    Se agrego un ejemplo mas de herencia

---
+++ Herencia/EldenPY/__pycache__/guerrero.py
@@ -0,0 +1,29 @@
+from personaje import Personaje
+
+class Guerrero(Personaje):
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, espada):
+        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+        self.espada = espada
+    
+    def cambiar_arma(self):
+        opcion = int(input("Elige un arma: (1) Acero inquebrantable, daño 8. (2) MataDragones, daño (10)"))
+        if opcion == 1:
+            self.espada = 8
+        elif opcion == 2:
+            self.espada = 10
+        else:
+            print("Numero de arma incorrecto")
+        
+        def atributos(self):
+            super().atributos()
+            print("Espada:", self.espada)
+
+        def daño(self, enemigo):
+            return self.fuerza*self.espada - enemigo.defensa
+        
+
+
+        
+guts = Guerrero("Guts", 20, 10, 10, 120, 5)
+guts.atributos()
+print(guts.espada)
